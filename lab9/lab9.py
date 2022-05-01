47595b024d9ab332393dc8bf084729446a98b8be
MbIUIb <kirill.chezlov@gmail.com>
2022-05-01 23:44:25 +0300

    optimization code after #отрисовка области

--- lab9/lab9.py
+++ lab9/lab9.py
@@ -198,47 +198,19 @@ def lab_8(circle_x=0, circle_y=0):
 
     # отрисовка области
     if circle_status == 'in' and rectangle_status == 'in':
-        if line_status == '>':
-            draw_line(line_x1, line_y1, line_x2, line_y2, '>')
-        elif line_status == '<':
-            draw_line(line_x1, line_y1, line_x2, line_y2, '<')
-        elif line_status == 'слева':
-            draw_line(line_x1, line_y1, line_x2, line_y2, 'слева')
-        elif line_status == 'справа':
-            draw_line(line_x1, line_y1, line_x2, line_y2, 'справа')
+        draw_line(line_x1, line_y1, line_x2, line_y2, line_status)
         draw_circle(circle_x, circle_y, circle_r, 'out', 'w')
         draw_rectangle(rectangle_x1, rectangle_y1, rectangle_x2, rectangle_y2, 'out', 'w')
     if circle_status == 'in' and rectangle_status == 'out':
-        if line_status == '>':
-            draw_line(line_x1, line_y1, line_x2, line_y2, '>')
-        elif line_status == '<':
-            draw_line(line_x1, line_y1, line_x2, line_y2, '<')
-        elif line_status == 'слева':
-            draw_line(line_x1, line_y1, line_x2, line_y2, 'слева')
-        elif line_status == 'справа':
-            draw_line(line_x1, line_y1, line_x2, line_y2, 'справа')
+        draw_line(line_x1, line_y1, line_x2, line_y2, line_status)
         draw_circle(circle_x, circle_y, circle_r, 'out', 'w')
         draw_rectangle(rectangle_x1, rectangle_y1, rectangle_x2, rectangle_y2, 'in', 'w')
     if circle_status == 'out' and rectangle_status == 'in':
-        if line_status == '>':
-            draw_line(line_x1, line_y1, line_x2, line_y2, '>')
-        elif line_status == '<':
-            draw_line(line_x1, line_y1, line_x2, line_y2, '<')
-        elif line_status == 'слева':
-            draw_line(line_x1, line_y1, line_x2, line_y2, 'слева')
-        elif line_status == 'справа':
-            draw_line(line_x1, line_y1, line_x2, line_y2, 'справа')
+        draw_line(line_x1, line_y1, line_x2, line_y2, line_status)
         draw_rectangle(rectangle_x1, rectangle_y1, rectangle_x2, rectangle_y2, 'out', 'w')
         draw_circle(circle_x, circle_y, circle_r, 'in', 'w')
     if circle_status == 'out' and rectangle_status == 'out':
-        if line_status == '>':
-            draw_line(line_x1, line_y1, line_x2, line_y2, '>')
-        elif line_status == '<':
-            draw_line(line_x1, line_y1, line_x2, line_y2, '<')
-        elif line_status == 'слева':
-            draw_line(line_x1, line_y1, line_x2, line_y2, 'слева')
-        elif line_status == 'справа':
-            draw_line(line_x1, line_y1, line_x2, line_y2, 'справа')
+        draw_line(line_x1, line_y1, line_x2, line_y2, line_status)
         draw_rectangle(rectangle_x1, rectangle_y1, rectangle_x2, rectangle_y2, 'in', 'w')
         draw_circle(circle_x, circle_y, circle_r, 'in', 'w')
 

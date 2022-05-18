b85f1228eb709677b532f8dd818cc2e24d8b3aaf
Krai53n <greasha46@gmail.com>
2022-05-18 13:32:52 +0300

    improve lab by using pathlib

--- Lab10/decrypt_lab9.py
+++ Lab10/decrypt_lab9.py
@@ -1,5 +1,6 @@
+from pathlib import Path
 import lab10
 
-with open('D:\Lab\programming_languages_labs\Lab9\Data\key') as f:
-    key = f.read()
-lab10.decrypt('D:\Lab\programming_languages_labs\Lab9\Data\lab9_logs.txt', 'Data\\decryptlogs', key)
+data_of_lab9_path = Path('../Lab9/Data')
+key = (data_of_lab9_path /  'key').read_text()
+lab10.decrypt(data_of_lab9_path / 'lab9_logs.txt', 'Data/decryptlogs', key)
